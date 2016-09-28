5463f09c0f0b0be842b82a318f54e0459bc0f722
Arthur Attwell <arthur@arthurattwell.com>
2016-09-28 10:22:00 +0200

    Add troubleshooting on gh-pages subtree issue

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -7,3 +7,12 @@ then
   exit 1
 fi
 git subtree push --prefix $1 origin gh-pages
+
+# If you're getting git errors about the remote behind behind:
+# 1. On GitHub, change your default branch to something other than gh-pages.
+# (You'll change it back soon.)
+# 2. Delete the gh-pages branch.
+# 3. Locally, run the command again:
+# git subtree push --prefix $1 origin gh-pages
+# where $1 is the folder you're pushing to gh-pages.
+# 4. On GitHub.com, change your default branch back to gh-pages.
